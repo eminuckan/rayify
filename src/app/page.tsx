03f9e3a9c671bc425ca30f68f735d3bd34cf7c24
eminuckan <m.eminuckan@gmail.com>
2023-10-17 19:26:09 +0300

    Listen component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import Listen from "components/components/shared/SpeechRecognition/Listen";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Rayify
+      <Listen />
     </main>
   )
 }
