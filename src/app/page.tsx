5b23196e7503c23fd2c7362c2072ea2fddc99d3a
eminuckan <m.eminuckan@gmail.com>
2023-10-30 21:22:20 +0300

    kayıt ve login sayfası işlemleri

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
-import Listen from "components/components/shared/SpeechRecognition/Listen";
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText, Input,
-} from '@chakra-ui/react';
+    Box,
+    Heading,
+    Container,
+    Text,
+    Stack,
+} from '@chakra-ui/react'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Listen />
-        <FormControl>
-            <FormLabel>Email address</FormLabel>
-            <Input className="border-2 border-neutral-800 hover:border-red-500 transition-colors duration-500 ease-in-out" type='email' />
-            <FormHelperText>We'll never share your email.</FormHelperText>
-        </FormControl>
+        <>
+            <Container maxW={'3xl'}>
+                <Stack
+                    as={Box}
+                    textAlign={'center'}
+                    spacing={{ base: 8, md: 14 }}
+                    py={{ base: 20, md: 36 }}>
+                    <Heading
+                        fontWeight={600}
+                        fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+                        lineHeight={'110%'}>
+                        Rayify <br />
+                        <Text as={'span'} color={'green.400'}>
+                            Engelsiz Müzik
+                        </Text>
+                    </Heading>
+                </Stack>
+            </Container>
+        </>
     </main>
   )
 }
+
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,13 +1,12 @@
+"use client"
+import 'regenerator-runtime'
 import {
     Box,
     Button,
-    Checkbox,
     Container,
-    Divider,
     FormControl,
     FormLabel,
     Heading,
-    HStack,
     Input,
     Link,
     Stack,
@@ -15,15 +14,83 @@ import {
 } from '@chakra-ui/react'
 import Logo from '../../components/shared/Logo/Logo';
 import PasswordField from "components/components/shared/Form/Input/PasswordInput";
+import {useEffect, useState, useRef, FormEvent} from "react";
+import SpeechRecognition, {useSpeechRecognition} from "react-speech-recognition";
+import {createUtter} from "../../utils/utter";
+import {useRouter} from "next/navigation";
+
 export default function Login ()
 {
+    const router = useRouter();
+    const messages = [
+        "Üye olmak için kullanıcı adınızı girin. Parola girişi için parola diyebilirsiniz. Tekrar dinlemek için tekrar diyebilirsiniz.",
+        "Parolanızı yazın ardından enter ile kayıt olun. Kullanıcı adını düzenlemek için kullanıcı diyebilirsiniz."
+    ];
+    const [speechRecognitionSupported, setSpeechRecognitionSupported] =
+        useState(false);
+    const [message, setMessage] = useState<string>(messages[0]);
+    const usernameInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
+    const {
+        transcript,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    const speakText = () =>{
+        const {synth, utter} = createUtter();
+        utter.text = message;
+        synth.cancel();
+        synth.speak(utter);
+    }
+
+    useEffect(() => {
+        setSpeechRecognitionSupported(browserSupportsSpeechRecognition)
+    }, [browserSupportsSpeechRecognition])
+
+    useEffect(() => {
+        speakText();
+        resetTranscript();
+    }, [message]);
+    const startListening = () => speechRecognitionSupported ? SpeechRecognition.startListening({language: 'tr', continuous:false}) : false;
+
+    useEffect(() => {
+        usernameInputRef.current?.focus();
+    }, []);
+
+    useEffect(() => {
+        startListening();
+        switch (transcript.trim().toLowerCase()) {
+            case "tekrar":
+                speakText();
+                break;
+            case "parola":
+                passwordInputRef.current?.focus();
+                setMessage(messages[1]);
+                break;
+            case "kullanıcı":
+                usernameInputRef.current?.focus();
+                setMessage(messages[0]);
+                break;
+            default:
+                break;
+        }
+    });
+
+    const handleRegister = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setMessage("Başarılı bir şekilde kayıt yaptınız, ana sayfaya yönlendiriliyorsunuz.")
+        router.push("/");
+        //back ende api call yapılacak
+    }
+
     return (
         <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
             <Stack spacing="8">
                 <Stack spacing="6" textAlign="center" className="flex items-center justify-center">
                     <Logo />
                     <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-                        <Heading size={{ base: 'xs', md: 'sm' }}>Hemen üye ol</Heading>
+                        <Heading size={{ base: 'sm', md: 'md' }}>Hemen üye ol</Heading>
                         <Text color="fg.muted">
                             Hesabın var mı ? <Link href="/login" className="text-blue-500 font-semibold">Giriş yap</Link>
                         </Text>
@@ -36,18 +103,20 @@ export default function Login ()
                     boxShadow={{ base: 'none', sm: 'md' }}
                     borderRadius={{ base: 'none', sm: 'xl' }}
                 >
-                    <Stack spacing="6">
-                        <Stack spacing="5">
-                            <FormControl>
-                                <FormLabel htmlFor="username">Kullanıcı adı</FormLabel>
-                                <Input id="username" type="text" />
-                            </FormControl>
-                            <PasswordField />
-                        </Stack>
+                    <form method="POST" onSubmit={handleRegister}>
                         <Stack spacing="6">
-                            <Button colorScheme='blue' variant="solid">Üye ol</Button>
+                            <Stack spacing="5">
+                                <FormControl>
+                                    <FormLabel htmlFor="username">Kullanıcı adı</FormLabel>
+                                    <Input required={true} ref={usernameInputRef} id="username" type="text" />
+                                </FormControl>
+                                <PasswordField ref={passwordInputRef} />
+                            </Stack>
+                            <Stack spacing="6">
+                                <Button type="submit" colorScheme='blue' variant="solid">Üye ol</Button>
+                            </Stack>
                         </Stack>
-                    </Stack>
+                    </form>
                 </Box>
             </Stack>
         </Container>
